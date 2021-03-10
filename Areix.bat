2fbd60fd75d59a8110f72f480b2abe10c4bd5f96
Mob <41581781+LazyChicken21@users.noreply.github.com>
2021-03-10 22:42:16 +0000

    The dosser

---
+++ Areix.bat
@@ -0,0 +1,95 @@
+@echo off
+color 4
+title Areix Dosser
+cls
+echo Welcome
+echo.
+echo.
+echo Dev by Mob
+echo.
+echo.
+@echo off
+color 4
+echo
+echo
+echo
+echo                                     +hhhhhhhhh+                `..` `dMMd..-`                      
+echo                                     syyyyyyhMM:       -oyhy+. -NMMm-/MMh.hMMN+                     
+echo                                            sMd`      /NNo/yMm-.hNNh-shs``smNd/                     
+echo                                            yd+       sMm.`/NM:  ``         `                       
+echo                                                     `smmmmd/   ommmmmmmmms                        
+echo                                     `hmh            `.-::.`    +ooooooyMM/                        
+echo                                     /MMo            .smNNo`           oMm`                        
+echo                                    yNm.              .yNNy.          sy+                         
+echo                                      ...             `.  .:.`                                      
+echo                                    `--:-.          +mm//hm+     .sys.                             
+echo                                 `/hmNNNNNh-        .hMMMd:` `ss+:dmd/ss+`                         
+echo                                 `yMNsoo++MMh`      -yNmmMN/  `dMNhsyhmMd:                          
+echo                                 `--.+NMm.---       :y/.`ss-   .+ydddhs/`                           
+echo                                     `::-                         ``.`                              
+echo                                  `/ooo/`         /ss.   :ss:    -ohh-                              
+echo                                   `+dMMy.        oMMdooyNNy`   +NMy.                               
+echo                                     `+yyo`       `/ydddho-     dMMN/                               
+echo                                 .........`          ```        -syo.                               
+echo                                /mmmmmmmmm/       `+yhhy+` `---.  ----`                             
+echo                               -/////////`       dMh//dMy .sNMN+`/mMMy`                            
+echo                                  `sss+.          NMo.-yMd   :dMMy..yNMd-                           
+echo                                  .osMMd`         -ydmmds.    `+oo:  :oo/`                          
+echo                                 `+smd+           ``..`    -oyo-  `/ss+`                           
+echo                                 :ys.            :do-`    .mMMMN` :MMMMs                           
+echo                            .-:-. `-::.           .omNh:    /hdh/  `oddy-                           
+echo                           -hMMd:`sNMN+`        `sdmdo.                                            
+echo                              +mMN+ :dMMy`       +y/.           .++:                                
+echo                               .+o+- `+o+:                      +NNm. `                             
+echo                            `ohhy- :yhh+       +hh+  `:yhy- .+sso+:-:hd+  -ss:    :yy/              
+echo              ``````         `oNMN+ /mMMy`     .dMMs/hMNs-+hNMNdNMNNMNy`  :NMNsoohNMy`              
+echo            .hMMMMMM+          -hMMy``sNMd.     `hMMMNy-/mMMmo.:++:/:`     -shmmdho-                
+echo           .dMMMMMMMh                                `/mMMd/` /MMN:                                 
+echo          :mMMMdNMMMN`                                ````     .-.                                  
+echo         /NMMMd`hMMMM:       :ssss/ :yddo    `/syddddy+-     +ssss:   `ossss+     -sssss:           
+echo        +NMMMh` oMMMMs       dMMMMsyMMMM/  :hNMMMMMMMMMMs   -NMMMM-    +MMMMN:  `oNMMMm/            
+echo       sMMMMy`  :MMMMd      .MMMMMNNhyyy``sMMMMdo-`:NMMMM.  oMMMMd`     sMMMMd -dMMMNo`       `..`  
+echo     `yMMMMy`   .NMMMM.     oMMMMMs. `/ydmMMMMmNNo:yMMMMd``.mMMMM+` ````.hMMMMyNMMNy.`ohmds:`.yNm.  
+echo    `hMMMMMdhhhhdNMMMM/    `mMMMMs   yMMmMMMMMMMMMMMMNd+:NNNMMMMMNs-NNNNNMMMMMMMMm: .dMNdmMMNNMN+   
+echo   .dMMMMMMMMMMMMMMMMMy    /MMMMm`  :dm/+MMMMmysso+/-`` .//NMMMMy/..////sMMMMMMMMN- omd-``-oss+.    
+echo  -mMMMMmhhhhhhhhmMMMMm`  `hMMMMo    `` :MMMMm-      `-   -NMMMM:     .yNMMMy/MMMMm-```             
+echo /mMMMMd.        /MMMMM-  -MMMMN-       `yMMMMmyoooydmo   :NMMMMmdy``omMMMm+` yMMMMd.               
+echo .mMMMMo  yMMMMh         `+dNMMMMMMMNh.   `yMMMMMMy/dMMMMh-   .mMMMMh`              
+echo ......`           .....`  .....`           `.:omMm+-.`   .--:/++ohs-.....`     `.....               
+echo  ` ::::::::::::::::::::::::::::/o:::::::::::::/dMN+:::::/hNNNmy++dMN/::::::::yds::::::::             
+echo  :ddddddddddddddddddddddddNNNmddddddddddddddmNMMMNNdddmMMmddmNMMNddddddNNNNMMMddddddddd`            
+echo  ```````````````````````:mMNo-`````````````./NMmdh.``/so````.::.``````syyyNMd`````````             
+echo                          `:ym+              -ys.`        `-:-            `sh/                      
+echo                          `--:`                          -dmh:     `+o- `o+` -o+                    
+echo                          +NN:                           yMs.      `dMh+hMNsomN/                    
+echo                         `dMm`                           :dmo       .oyso:oys+.                     
+echo                         -dd+                             `-...`     .:    .:                       
+echo                          ``                            ````:mm-  `-smd++++yNy.                     
+echo           :yyyyyyyyyyyyyyy:                           `ddddNMd   .hMMmmmmmMNd:                     
+echo           -+++++++++++++++-                           .+++sMM+    `+s````:s-`                      
+echo           `---------------`                          ```` -oo/:.                                   
+echo           /ddddddddddddddd/                        `+hmmds:-oNM/                                   
+echo            `````````````..`                       `yMNhymNMMMNo                                    
+echo                       .ymms                       -hh/  `:++/.                                     
+echo                      `yNMd                           `-   ..                                      
+echo                       `sh+`                          -dMy/yNh`                                     
+echo                        ` `..                          +MMMN/`                                      
+echo                    ````.hMh                        .sNNhMMy`                                      
+echo                     oNNNNMM/                        `o+` :s-                                       
+echo
+echo
+echo Please enter in an URL/IP to Ping.
+echo.
+set /p url=URL/IP: 
+echo.
+echo Please enter in the power that you wish.
+echo.
+set /p power=Power (1-65500): 
+echo.
+echo.
+echo.
+echo Starting ping...
+echo.
+ping %url% -t -l %power%
+pause
+
